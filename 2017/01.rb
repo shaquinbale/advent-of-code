f5e57a998e0886c333811912f0cc36117f1b5cbd
shaquinbale <scopelenskingdra@gmail.com>
2024-11-09 13:52:48 -0500

    Finish part 2 of day 1

--- 2017/01.rb
+++ 2017/01.rb
@@ -1,4 +1,4 @@
-def sum_from_index (input)
+def part_one (input)
   sum = 0
   (0..input.length).each do |index|
     sum += input[index].to_i if input[index] == input[(index + 1) % input.length]
@@ -6,6 +6,18 @@ def sum_from_index (input)
   sum
 end
 
+def part_two (input)
+  sum = 0
+  half_length = input.length / 2
+  (0..input.length).each do |index|
+    sum += input[index].to_i if input[index] == input[(index + half_length) % input.length]
+  end
+  sum
+end
+
+
 File.open('01.txt', 'r') do |file|
-  puts sum_from_index(file.readline.chomp)
+  input = file.readline.chomp
+  puts part_one(input)
+  puts part_two(input)
 end
