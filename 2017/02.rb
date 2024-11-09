a1d451e31aed1e03ab00a89ce8e6b63f43a2fb2e
shaquinbale <scopelenskingdra@gmail.com>
2024-11-09 14:21:52 -0500

    Finish day 2 part 2

--- 2017/02.rb
+++ 2017/02.rb
@@ -1,15 +1,32 @@
 def part_one(lines)
   checksum = 0
-
   lines.each do |line|
     array =  line.split.map(&:to_i)
     checksum += array.max - array.min
   end
+  checksum
+end
 
+def part_two (lines)
+  
+  checksum = 0
+
+  lines.each do |line|
+    array =  line.split.map(&:to_i)
+    array.each_with_index do |num1, index1|
+      array.each_with_index do |num2, index2|
+        next if index1 == index2
+        if num1 % num2 == 0
+          checksum += num1 / num2
+        end
+      end
+    end
+  end
   checksum
 end
 
 File.open('02.txt', 'r') do |file|
   lines = file.readlines.map
   puts part_one(lines)
+  puts part_two(lines)
 end
