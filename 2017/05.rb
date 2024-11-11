2f27edc6ad0d57fae673804bdcb1cf100cdad07f
shaquinbale <scopelenskingdra@gmail.com>
2024-11-10 20:47:10 -0500

    Finish day 5 part 2
    This was a literal nightmare. Rewrote my code
    several times and it didn't work until it did.
    Still have no clue what was wrong with it

--- 2017/05.rb
+++ 2017/05.rb
@@ -16,7 +16,31 @@ def part_one (array)
   steps
 end
 
+def part_two(array)
+  steps = 0
+  index = 0
+
+  loop do
+    break if index < 0 || index >= array.length
+
+    jmp = array[index]
+    new_index = index + jmp
+
+    if jmp >= 3
+      array[index] -= 1
+    else
+      array[index] += 1
+    end
+
+    index = new_index
+    steps += 1
+  end
+
+  steps
+end
+
 File.open('05.txt', 'r') do |file|
   lines = file.readlines(chomp: true).map(&:to_i)
   puts part_one(lines)
+  puts part_two(lines)
 end
