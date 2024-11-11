637d50acb24af073599f56dc349bebc560dd7249
shaquinbale <scopelenskingdra@gmail.com>
2024-11-11 09:46:14 -0500

    Finish part 2 and fix part 1
    Replaced arrays with sets for searching for a
    duplicate

--- 2017/06.rb
+++ 2017/06.rb
@@ -1,17 +1,10 @@
-=begin
-  Increment cycle counter
-  Break if this array is a duplicate
-  Note the current banks on our array
-  Find bank with most blocks b (tie break by lowest index)
-  Distribute 1 to each block after the current b times
-=end
+require 'set'
 
 def part_one(banks)
   cycle = 0
-  seen = []
-  loop do
-    break if seen.include?(banks) # Break if this array is a duplicate
-    seen << banks.dup # Add the current banks to our array on our array
+  seen = Set.new
+  until seen.include?(banks)
+    seen.add(banks.dup)
 
     # Find bank with most blocks (tie break by lowest index)
     max_value = banks.max
@@ -28,7 +21,31 @@ def part_one(banks)
   cycle
 end
 
+def part_two(banks)
+  cycle = 0
+  seen = {}
+  
+  until seen.include?(banks)
+    seen[banks.dup] = cycle
+
+    max_value = banks.max
+    distributor_index = banks.index(max_value)
+    distribute_amount = banks[distributor_index]
+    banks[distributor_index] = 0
+
+    distribute_amount.times do |i|
+      banks[(distributor_index + i + 1) % banks.size] += 1
+    end
+    
+    cycle += 1
+  end
+
+  loop_start_cycle = seen[banks]
+  cycle - loop_start_cycle
+end
+
 File.open('06.txt', 'r') do |file|
   banks = file.readline.split.map(&:to_i)
   puts part_one(banks)
+  puts part_two(banks)
 end
